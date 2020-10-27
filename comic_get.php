a7c34c237421a3399ba574401db3f7dab762da73
Ashley Tang <atang10@binghamton.edu>
2020-10-27 12:20:44 -0400

    Opens up a new window to crop picture as well as make a cropping box

---
+++ comic_get.php
@@ -0,0 +1,21 @@
+<?php
+    $dh = "comics"; // all comic pictures in the comic directory
+    $comic = "comic_1.jpg";
+    //Checks whole directory for comic picture wanted
+    if(is_dir($dh)){
+        $search_dir = opendir($dh);
+        while($file = readdir($search_dir)){
+            if($file == $comic){
+		    //echo $file . "<br>";
+                $loc = getcwd() . '/comics/';
+                $copied = $loc . "copy_" . $file;
+		//echo $loc;
+                copy($loc . $file, $copied);
+                echo "comics/" . "copy_" . $file;
+            }
+        }
+        closedir($search_dir);
+    }
+
+
+?>
